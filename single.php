a6133e50c8cf795b2fce562c61481062452ce710
mohaamadsadegh <sadeghloveymi2@gmail.com>
2025-06-25 15:43:02 +0330

    add frist commit

--- single.php
+++ single.php
@@ -288,6 +288,7 @@ while (have_posts()) : the_post();
             <?php
             if ( comments_open() || get_comments_number() ) :
                 comments_template();
+//             TODO TEST GITHUB
             endif;
             ?>
             <!-- فرم ارسال دیدگاه -->
